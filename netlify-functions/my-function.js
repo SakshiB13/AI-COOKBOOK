70794201b8c117f436cdbfbeda7981d0ede28b52
Sakshi Bole <98754992+SakshiB13@users.noreply.github.com>
2023-05-05 17:04:30 +0530

    Add files via upload

---
+++ netlify-functions/my-function.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+require("dotenv").config();
+
+
+const { Configuration, OpenAIApi } = require("openai");
+
+
+const configuration = new Configuration({
+  apiKey:process.env.OPENAI_API_KEY
+});
+const openai = new OpenAIApi(configuration);
+
+
+app.post("/recipegenerator", async (req, res) => {
+  promptText = req.body.prompt;
+  console.log(promptText);
+  try {
+    if (promptText == null) {
+      throw new Error("Uh oh, no prompt was provided");
+    }
+  
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "Write a recipe based on these ingredients and instructions:" + promptText,
+      temperature: 0.3,
+      max_tokens: 500,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+    const completion=response.data.choices[0].text;
+    recipe=completion;
+   console.log(recipe);
+  } catch (error) {
+    console.log(error.message);
+  }
+  res.redirect("/recipe");
+});
+
+module.exports.handler = async (event, context) => {
+  return await app(event, context)
+}
